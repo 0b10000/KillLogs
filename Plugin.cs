6985047c623a86550ae484fd149b9e0d376932fe
0b10000 <zerob10000@tutanota.com>
2021-12-30 21:39:58 -0500

    bump version

--- Plugin.cs
+++ Plugin.cs
@@ -18,8 +18,8 @@ namespace KillLogs
         public override string Author => "0b10000";
         public override string Name => "KillLogs";
         public override string Prefix => "KillLogs";
-        public override Version Version { get; } = new(2, 0, 1);
-        public override Version RequiredExiledVersion { get; } = new(4, 1, 5);
+        public override Version Version { get; } = new(2, 0, 2);
+        public override Version RequiredExiledVersion { get; } = new(4, 1, 7);
 
         private EventHandlers EventHandlers { get; set; }
 
